b74b910cda6f47ffe55f5076a5bdeda89d6f78ce
八雲 正邪 <30519243+Seizya@users.noreply.github.com>
2018-07-27 15:46:49 +0900

    Add files via upload

--- Main menu/main menu.js
+++ Main menu/main menu.js
@@ -14,12 +14,26 @@ var menu = document.getElementById("menu");
 var clickb = false;
 menu.addEventListener('click', () => {
     clickb = !clickb;
-    console.log(clickb)
+    //console.log(clickb)
     if (clickb) {
         document.getElementById("menu").className = "hover";
         document.getElementById("menus").className = "hover";
     } else {
         document.getElementById("menu").className = "";
         document.getElementById("menus").className = "";
+    }
+}, false);
+
+var bg_chenge = document.getElementById("bg_chenge");
+var clickc = true;
+bg_chenge.addEventListener('click', () => {
+    clickc = !clickc;
+    console.log(clickc)
+    if (clickc) {
+        document.getElementById("bg").className = "one";
+        document.getElementById("circle2").className = "one";
+    } else {
+        document.getElementById("bg").className = "two";
+        document.getElementById("circle2").className = "two";
     }
 }, false);

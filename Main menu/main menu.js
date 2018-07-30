c6b7570efd2469a8da06ea194c60179f7e40fce4
八雲 正邪 <30519243+Seizya@users.noreply.github.com>
2018-07-30 22:49:37 +0900

    Add files via upload

--- Main menu/main menu.js
+++ Main menu/main menu.js
@@ -28,12 +28,35 @@ var bg_chenge = document.getElementById("bg_chenge");
 var clickc = true;
 bg_chenge.addEventListener('click', () => {
     clickc = !clickc;
-    console.log(clickc)
     if (clickc) {
         document.getElementById("bg").className = "one";
         document.getElementById("circle").className = "one";
+        document.getElementById("line_x").className = "one";
+        document.getElementById("line_y").className = "one";
     } else {
         document.getElementById("bg").className = "two";
         document.getElementById("circle").className = "two";
+        document.getElementById("line_x").className = "two";
+        document.getElementById("line_y").className = "two";
+    }
+}, false);
+
+var gear_chenge = document.getElementById("gear_chenge");
+var clickd = true;
+gear_chenge.addEventListener('click', () => {
+    clickd = !clickd;
+    console.log(clickd)
+    if (clickd) {
+        document.getElementById("smbg2").className = "on";
+        document.getElementById("gear1").className = "off";
+        document.getElementById("gear2").className = "off";
+        document.getElementById("gear3").className = "off";
+        document.getElementById("gear4").className = "off";
+    } else {
+        document.getElementById("smbg2").className = "off";
+        document.getElementById("gear1").className = "on";
+        document.getElementById("gear2").className = "on";
+        document.getElementById("gear3").className = "on";
+        document.getElementById("gear4").className = "on";
     }
 }, false);

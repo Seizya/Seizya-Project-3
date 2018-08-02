52e2207d801d485074cb5bd514031b2137b9a8e8
八雲 正邪 <30519243+Seizya@users.noreply.github.com>
2018-08-03 01:10:44 +0900

    Add files via upload

--- Sab menu/index.js
+++ Sab menu/index.js
@@ -1,8 +1,37 @@
+//document.getElementById('level').scrollTop = document.getElementById('level').scrollHeight;
 var of=false;
+var ofa=false;
+var link;
 
-var left=document.getElementById('left');
-left.addEventListener('click',()=>{
-	of=!of;
-	document.getElementById('left').className=String(of);
-	document.getElementById('right').className=String(of);
+document.getElementById('menu').addEventListener('click',()=>{
+	ofa=!ofa;
+	document.getElementById('menus').className=String(ofa);
 })
+
+document.getElementById('right-close').addEventListener('click',()=>{
+	document.getElementById('right').className=String(false);
+})
+
+document.getElementById('lv1').addEventListener('click',()=>{
+	starts("../Brain God/index.html");
+})
+
+document.getElementById('lv2').addEventListener('click',()=>{
+	starts("../Brain G/index.html");
+})
+
+document.getElementById('lv3').addEventListener('click',()=>{
+	starts("../Brain Good/index.html");
+})
+
+function starts(link){
+	document.getElementById('right').className="true";
+	document.getElementById('start').addEventListener('click',()=>{
+		document.getElementById('load').className="true";
+		setTimeout(()=>{
+			document.location.href=link;
+			document.getElementById('load').className="";
+			document.getElementById('right').className="";
+		},5000)
+	})	
+}
